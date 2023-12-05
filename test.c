0016aa5e5ce981b3c0d4c00ededf8b94351c89dd
sangjiwei <s2792945826@outlook.com>
2023-12-05 10:06:03 +0800

    aa

--- test.c
+++ test.c
@@ -8,3 +8,6 @@ int main(int argc, char const *argv[])
 
     return 0;
 }
+Failed to connect to repository : Command "D:\Software\Programs\Git\bin\git.exe ls-remote -h -- https://github.com/sangjiwei/testgit.git HEAD" returned status code 128:
+stdout:
+stderr: fatal: unable to access 'https://github.com/sangjiwei/testgit.git/': SSL certificate problem: unable to get local issuer certificate
